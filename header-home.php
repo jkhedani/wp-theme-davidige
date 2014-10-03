ca795607ee8884b8aef221a4c931d699a09f7592
Justin Hedani <jkhedani@gmail.com>
2014-10-03 10:55:52 -1000

    Ensure blog post count durability.

--- header-home.php
+++ header-home.php
@@ -40,82 +40,74 @@
 
 <body <?php body_class(); ?>>
 
-<a id="top"></a>
+  <a id="top"></a>
 
-<!-- HEADER -->
-<div id="header" class="width100" <?php tb_write_bckg(); ?>>
+  <!-- HEADER -->
+  <div id="header" class="width100" <?php tb_write_bckg(); ?>>
     <div class="width1000">
-
-        <div id="logo"><h1><a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><img src="<?php echo tb_get_logo(); ?>" alt="<?php bloginfo('name'); ?>"></a></h1></div>
-
+      <div id="logo"><h1><a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><img src="<?php echo tb_get_logo(); ?>" alt="<?php bloginfo('name'); ?>"></a></h1></div>
     </div>
-</div>
-<!-- .HEADER -->
+  </div>
+  <!-- .HEADER -->
 
-<div class="clear"></div>
+  <div class="clear"></div>
 
-<!-- CONTENT -->
-<div id="contentHolder" class="width100">
+  <!-- CONTENT -->
+  <div id="contentHolder" class="width100">
 
-	<!-- Navigation -->
+  	<!-- Navigation -->
     <div id="navigationBckg" class="<?php tb_write_bckg('navigation'); ?> width100">
-
-        <div id="navigation">
-
-		<div class="width1000">
-		<?php
-			if (has_nav_menu('Navigation')) {
-		        wp_nav_menu(
-		            array(
-		                'theme_location' => 'Navigation',
-		                'container' => false,
-		                'menu_class' => 'navigation',
-		                'fallback_cb' => 'tb_default_navigation'
-		            )
-		        );
-			} else {
-				tb_default_navigation();
-			}
-	    ?>
+      <div id="navigation">
+  		  <div class="width1000">
+    		<?php
+    			if (has_nav_menu('Navigation')) {
+    		        wp_nav_menu(
+    		            array(
+    		                'theme_location' => 'Navigation',
+    		                'container' => false,
+    		                'menu_class' => 'navigation',
+    		                'fallback_cb' => 'tb_default_navigation'
+    		            )
+    		        );
+    			} else {
+    				tb_default_navigation();
+    			}
+  	    ?>
         </div>
-
-        </div>
-
+      </div>
     </div>
-	<!-- .Navigation -->
+  	<!-- .Navigation -->
 
     <!-- MAIN -->
     <div id="main" class="width100">
 
-		<?php
-		global $post;
-		$postID = $post->ID;
-		$mainImageA = get_post_meta($postID, '_main_image', false);
-		$mainImage = false;
-		foreach ($mainImageA as $tempImg) {
-			if (wp_get_attachment_image_src($tempImg)) {
-				$mainImage = $tempImg;
-				break;
-			}
-		}
+      <?php
+  		global $post;
+  		$postID = $post->ID;
+  		$mainImageA = get_post_meta($postID, '_main_image', false);
+  		$mainImage = false;
+  		foreach ($mainImageA as $tempImg) {
+  			if (wp_get_attachment_image_src($tempImg)) {
+  				$mainImage = $tempImg;
+  				break;
+  			}
+  		}
+  		$mainShadow = get_post_meta($postID, '_main_shadow', true);
+  		$mainImageArray = tb_get_main_image($mainImage, $mainShadow);
+  		$sidebarPosition = get_post_meta($postID, '_sidebar_position', true);
+  		?>
 
-		$mainShadow = get_post_meta($postID, '_main_shadow', true);
-		$mainImageArray = tb_get_main_image($mainImage, $mainShadow);
-		$sidebarPosition = get_post_meta($postID, '_sidebar_position', true);
-		?>
+  		<!-- Promo Image -->
+  		<div id="promoImage" class="width100">
+  		  <?php if ($mainImageArray['mainImage']) { ?>
+  			<div style="background-image: url('<?php echo $mainImageArray['mainImage']; ?>');">
+  				<img src="<?php echo $mainImageArray['mainImage']; ?>">
+  				<?php if ($mainImageArray['mainShadow'] == 'yes') echo '<div class="mainShadow"></div>'; ?>
+  			</div>
+  			<?php } ?>
+  		</div>
+  		<!-- .Promo Image -->
 
-		<!-- Promo Image -->
-		<div id="promoImage" class="width100">
-			<?php if ($mainImageArray['mainImage']) { ?>
-			<div style="background-image: url('<?php echo $mainImageArray['mainImage']; ?>');">
-				<img src="<?php echo $mainImageArray['mainImage']; ?>">
-				<?php if ($mainImageArray['mainShadow'] == 'yes') echo '<div class="mainShadow"></div>'; ?>
-			</div>
-			<?php } ?>
-		</div>
-		<!-- .Promo Image -->
-
-
-        <!-- Content -->
-        <div id="content" class="<?php tb_write_bckg('buttons'); ?> <?php tb_write_bckg('buttonsExtra'); ?>  <?php tb_write_bckg('sidebar'); ?> <?php echo tb_get_sidebar_position($sidebarPosition); ?> width1000" <?php if (!$mainImageArray['mainImage']) echo ' style="margin-top: 0 !important;"'; ?>>
-		<div class="fullWidth">
+      <!-- Content -->
+      <div id="content" class="<?php tb_write_bckg('buttons'); ?> <?php tb_write_bckg('buttonsExtra'); ?>  <?php tb_write_bckg('sidebar'); ?> <?php echo tb_get_sidebar_position($sidebarPosition); ?> width1000" <?php if (!$mainImageArray['mainImage']) echo ' style="margin-top: 0 !important;"'; ?>>
+  		    <div class="fullWidth">
